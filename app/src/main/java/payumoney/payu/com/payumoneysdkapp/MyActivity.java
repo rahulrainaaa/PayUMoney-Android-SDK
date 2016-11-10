c3a618cd916af5939c39216a34578a6d2bb675da
rahulrainahomeee <rahulrainahome@gmail.com>
2016-11-10 16:18:48 +0530

    Initial commit

---
+++ app/src/main/java/payumoney/payu/com/payumoneysdkapp/MyActivity.java
@@ -0,0 +1,260 @@
+package payumoney.payu.com.payumoneysdkapp;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.payUMoney.sdk.PayUmoneySdkInitilizer;
+import com.payUMoney.sdk.SdkCobbocEvent;
+import com.payUMoney.sdk.SdkConstants;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+
+public class MyActivity extends Activity {
+
+
+    EditText amt = null;
+    Button pay = null;
+
+    public static final String TAG = "PayUMoneySDK Sample";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        amt = (EditText) findViewById(R.id.amount);
+        pay = (Button) findViewById(R.id.pay);
+    }
+
+    private boolean isDouble(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private String getTxnId() {
+        return ("0nf7" + System.currentTimeMillis());
+    }
+
+    private double getAmount() {
+
+
+        Double amount = 10.0;
+
+        if (isDouble(amt.getText().toString())) {
+            amount = Double.parseDouble(amt.getText().toString());
+            return amount;
+        } else {
+            Toast.makeText(getApplicationContext(), "Paying Default Amount ₹10", Toast.LENGTH_LONG).show();
+            return amount;
+        }
+    }
+
+    public void makePayment(View view) {
+
+        PayUmoneySdkInitilizer.PaymentParam.Builder builder = new PayUmoneySdkInitilizer.PaymentParam.Builder();
+
+        builder.setAmount(getAmount())
+                .setTnxId(getTxnId())
+                .setPhone("7507326903")
+                .setProductName("product_name")
+                .setFirstName("rahul")
+                .setEmail("rahulrainahome1@gmail.com")
+                .setsUrl("https://www.payumoney.com/mobileapp/payumoney/success.php")
+                .setfUrl("https://www.payumoney.com/mobileapp/payumoney/failure.php")
+                .setUdf1("")
+                .setUdf2("")
+                .setUdf3("")
+                .setUdf4("")
+                .setUdf5("")
+                .setIsDebug(true)
+                .setKey("esv9nFf4")
+                .setMerchantId("5666893");// Debug Merchant ID
+
+        PayUmoneySdkInitilizer.PaymentParam paymentParam = builder.build();
+
+            /*
+             server side call required to calculate hash with the help of <salt>
+             <salt> is already shared along with merchant <key>
+             serverCalculatedHash =sha512(key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|<salt>)
+
+             (e.g.)
+
+             sha512(FCstqb|0nf7|10.0|product_name|piyush|piyush.jain@payu.in||||||MBgjYaFG)
+
+             9f1ce50ba8995e970a23c33e665a990e648df8de3baf64a33e19815acd402275617a16041e421cfa10b7532369f5f12725c7fcf69e8d10da64c59087008590fc
+
+            */
+
+        // Recommended
+        calculateServerSideHashAndInitiatePayment(paymentParam);
+
+           /*
+            testing purpose
+
+            String serverCalculatedHash="9f1ce50ba8995e970a23c33e665a990e648df8de3baf64a33e19815acd402275617a16041e421cfa10b7532369f5f12725c7fcf69e8d10da64c59087008590fc";
+            paymentParam.setMerchantHash(serverCalculatedHash);
+            PayUmoneySdkInitilizer.startPaymentActivityForResult(this, paymentParam);
+            */
+    }
+
+    public static String hashCal(String str) {
+        byte[] hashseq = str.getBytes();
+        StringBuilder hexString = new StringBuilder();
+        try {
+            MessageDigest algorithm = MessageDigest.getInstance("SHA-512");
+            algorithm.reset();
+            algorithm.update(hashseq);
+            byte messageDigest[] = algorithm.digest();
+            for (byte aMessageDigest : messageDigest) {
+                String hex = Integer.toHexString(0xFF & aMessageDigest);
+                if (hex.length() == 1) {
+                    hexString.append("0");
+                }
+                hexString.append(hex);
+            }
+        } catch (NoSuchAlgorithmException ignored) {
+        }
+        return hexString.toString();
+    }
+
+    private void calculateServerSideHashAndInitiatePayment(final PayUmoneySdkInitilizer.PaymentParam paymentParam) {
+
+        // Replace your server side hash generator API URL
+        String url = "https://test.payumoney.com/payment/op/calculateHashForTest";
+
+        Toast.makeText(this, "Please wait... Generating hash from server ... ", Toast.LENGTH_LONG).show();
+        StringRequest jsonObjectRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    if (jsonObject.has(SdkConstants.STATUS)) {
+                        String status = jsonObject.optString(SdkConstants.STATUS);
+                        if (status != null || status.equals("1")) {
+
+                            String hash = jsonObject.getString(SdkConstants.RESULT);
+                            Log.i("app_activity", "Server calculated Hash :  " + hash);
+
+                            paymentParam.setMerchantHash(hash);
+
+                            PayUmoneySdkInitilizer.startPaymentActivityForResult(MyActivity.this, paymentParam);
+                        } else {
+                            Toast.makeText(MyActivity.this,
+                                    jsonObject.getString(SdkConstants.RESULT),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                if (error instanceof NoConnectionError) {
+                    Toast.makeText(MyActivity.this,
+                            MyActivity.this.getString(R.string.connect_to_internet),
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MyActivity.this,
+                            error.getMessage(),
+                            Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return paymentParam.getParams();
+            }
+        };
+        Volley.newRequestQueue(this).add(jsonObjectRequest);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == PayUmoneySdkInitilizer.PAYU_SDK_PAYMENT_REQUEST_CODE) {
+
+            /*if(data != null && data.hasExtra("result")){
+              String responsePayUmoney = data.getStringExtra("result");
+                if(SdkHelper.checkForValidString(responsePayUmoney))
+                    showDialogMessage(responsePayUmoney);
+            } else {
+                showDialogMessage("Unable to get Status of Payment");
+            }*/
+
+
+            if (resultCode == RESULT_OK) {
+                Log.i(TAG, "Success - Payment ID : " + data.getStringExtra(SdkConstants.PAYMENT_ID));
+                String paymentId = data.getStringExtra(SdkConstants.PAYMENT_ID);
+                showDialogMessage("Payment Success Id : " + paymentId);
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.i(TAG, "failure");
+                showDialogMessage("cancelled");
+            } else if (resultCode == PayUmoneySdkInitilizer.RESULT_FAILED) {
+                Log.i("app_activity", "failure");
+
+                if (data != null) {
+                    if (data.getStringExtra(SdkConstants.RESULT).equals("cancel")) {
+
+                    } else {
+                        showDialogMessage("failure");
+                    }
+                }
+                //Write your code if there's no result
+            } else if (resultCode == PayUmoneySdkInitilizer.RESULT_BACK) {
+                Log.i(TAG, "User returned without login");
+                showDialogMessage("User returned without login");
+            }
+        }
+    }
+
+    private void showDialogMessage(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(TAG);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+}
